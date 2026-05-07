6078508ec1d733f49bc6b4a0a87f1dd51037dd14
D1eg0PH <diegoarmandopehu@gmail.com>
2026-05-07 14:04:33 -0400

    Update layout.tsx

--- clerk-nextjs/app/layout.tsx
+++ clerk-nextjs/app/layout.tsx
@@ -16,8 +16,8 @@ export const metadata: Metadata = {
   title: "Sergio Landscape Design LLC", // Esto es lo que sale en la pestaña
   description: "Professional gardening and landscaping services",
   icons: { 
-        icon: "/tree-sergio.png", 
-        apple: "/tree-sergio.png",
+        icon: "/icon.png", 
+        apple: "/icon.png",
     
     },
 };
