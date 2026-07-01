bbcedce0c3d793c4c0cb2ca375330c9786938671
diegoarmandope <diegoarmandopehu@gmail.com>
2026-07-01 17:23:44 -0600

    Stable version before theme

---
+++ clerk-nextjs/components/FireworksBackground.tsx
@@ -0,0 +1,9 @@
+export default function FireworksBackground() {
+  return (
+    <div className="absolute inset-0 overflow-hidden pointer-events-none">
+      <div className="firework firework1"></div>
+      <div className="firework firework2"></div>
+      <div className="firework firework3"></div>
+    </div>
+  );
+}
